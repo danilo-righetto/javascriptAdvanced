dd0cba2a739c9174c7d9db4b7677ff9260561981
Danilo <danilonewtrue@gmail.com>
2016-07-23 17:51:31 -0300

    Preparando para atualizar os registros

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ console.log(getTotal(list));
 function setList(list){
 	var table = '<thead> <tr> <td>Description</td> <td>Amount</td> <td>Value</td> <td>Action</td> </tr> </thead><tbody>';
 	for (var key in list) {
-		table += '<tr> <td>'+ formatDesc(list[key].desc)+'</td> <td>'+ list[key].amount+'</td> <td>'+ formatValue(list[key].value)+'</td> <td>Edit | Delete</td> </tr>';
+		table += '<tr> <td>'+ formatDesc(list[key].desc)+'</td> <td>'+ list[key].amount+'</td> <td>'+ formatValue(list[key].value)+'</td> <td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button>  | Delete</td> </tr>';
 	}
 	table += '</tbody>';
 	document.getElementById("listTable").innerHTML = table;
@@ -73,6 +73,27 @@ function addData(){
 	list.unshift({"desc": desc, "amount":amount, "value":value});
 
 	setList(list);
+}
+
+/* Implementando o botão Edit */
+function setUpdate(id){
+	var obj = list[id];
+	document.getElementById("desc").value = obj.desc;
+	document.getElementById("amount").value = obj.amount;
+	document.getElementById("value").value = obj.value;
+	document.getElementById("btnUpdate").style.display = "inline-block";
+	document.getElementById("btnAdd").style.display = "none";
+}
+
+/* resetForm() */
+function resetForm(){
+	document.getElementById("desc").value = "";
+	document.getElementById("amount").value = "";
+	document.getElementById("value").value = "";
+
+	document.getElementById("btnUpdate").style.display = "none";
+	document.getElementById("btnAdd").style.display = "inline-block";
+
 }
 
 /* Usando a funcao setList() */
